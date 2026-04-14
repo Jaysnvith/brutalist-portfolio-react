b0e1109f297e2f54e72acc9a1258858ae6e4c129
Jaysnvith <84651755+Jaysnvith@users.noreply.github.com>
2026-04-14 22:07:08 +0700

    feat: replace project display with image carousel menu

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -8,7 +8,6 @@ import wiredGunStore from "../assets/img/work-wired-gun-store.webp";
 import salesDashboard from "../assets/img/work-sales-dashboard.webp";
 import guestbook from "../assets/img/work-guestbook.webp";
 import Card from "../components/Card";
-import Cursor from "../components/Cursor";
 import { fadeIn } from "../animations/fadeIn";
 import { RevealText } from "../components/RevealText";
 
@@ -21,7 +20,9 @@ const projectImage = {
 } as const;
 
 function Projects() {
-  const [showDesc, setShowDesc] = useState<string | null>(null);
+  const [showProject, setShowProject] = useState<number>(0);
+  const [showButton, setShowButton] = useState<boolean>(false);
+  const [clickedButton, setClickedButton] = useState<'next' | 'previous' | null>(null)
 
   return (
     <div>
@@ -36,58 +37,6 @@ function Projects() {
         <Card className="px-2">MY PROJECTS</Card>
       </motion.div>
 
-      <div className="grid grid-cols-2 gap-6 mb-12">
-        {projectData.map(({ id, name, desc }) => {
-          const Image = projectImage[name as keyof typeof projectImage];
-          const activeDesc = showDesc === id;
-
-          return (
-            <motion.button
-              key={id}
-              className="flex flex-col items-center"
-              variants={popIn}
-              initial='hidden'
-              whileInView='visible'
-              transition={{
-                ease: 'linear',
-                duration: 0.3,
-                delayChildren: 0.3,
-              }}
-              onHoverStart={() => setShowDesc(id)}
-              onHoverEnd={() => setShowDesc(null)}
-            >
-              <Card>
-                <motion.img
-                  src={Image}
-                  alt={name}
-                  onClick={() => setShowDesc(activeDesc ? null : id)}
-                  variants={fadeIn}
-                />
-              </Card>
-
-              <AnimatePresence>
-                {activeDesc && (
-                  <motion.div
-                    className="mx-6"
-                    variants={popIn}
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                  >
-                    <Card>
-                      <p className="text-left">
-                        {desc}
-                        <Cursor />
-                      </p>
-                    </Card>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </motion.button>
-          );
-        })}
-      </div>
-
       <motion.div
         variants={popIn}
         initial='hidden'
@@ -99,7 +48,96 @@ function Projects() {
         }}
       >
         <Card>
-          <RevealText text="All data displayed are for demonstration purposes only." />
+          <motion.div
+            className="relative"          
+            variants={fadeIn}
+            onHoverStart={() => setShowButton(true)}
+            onHoverEnd={() => setShowButton(false)}
+          >
+            <AnimatePresence mode="wait">
+              <motion.img
+                key={showProject}
+                src={projectImage[projectData[showProject].name as keyof typeof projectImage]}
+                alt="Image"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            </AnimatePresence>
+            <AnimatePresence>
+              {showButton && (
+                <>                
+                  <motion.button 
+                    className="absolute top-4 left-4 bottom-4 w-1/3 border-2 font-bold text-2xl border-accent bg-accent/60 text-background hover:bg-accent/80 transition-colors"
+                    variants={{
+                      idle: { scale: 1, opacity: 1 },
+                      clicked: { scale: 1.5, opacity: 0 },
+                      hidden: { scale: 1, opacity: 0 },
+                    }}
+                    initial="hidden"
+                    animate={clickedButton === 'previous' ? 'clicked' : 'idle'}
+                    exit="hidden"
+                    onClick={() => {
+                      if (showProject === 0) {
+                        setShowProject(projectData.length - 1)
+                      } else {
+                        setShowProject(showProject - 1)
+                      }
+                      setClickedButton('previous')
+                    }}
+                    onAnimationComplete={(definiton) => {
+                      if(definiton === 'clicked') setClickedButton(null)
+                    }}
+                  >
+                    P R E V I O U S
+                  </motion.button>
+                  <motion.button
+                    className="absolute top-4 right-4 bottom-4 border-2 w-1/3 font-bold text-2xl border-accent bg-accent/60 text-background hover:bg-accent/80 transition-colors"
+                    variants={{
+                      idle: { scale: 1, opacity: 1 },
+                      clicked: { scale: 1.5, opacity: 0 },
+                      hidden: { scale: 1, opacity: 0 },
+                    }}
+                    initial="hidden"
+                    animate={clickedButton === 'next' ? 'clicked' : 'idle'}
+                    exit="hidden"
+                    onClick={() => {
+                      if (showProject === projectData.length - 1) {
+                        setShowProject(0)
+                      } else {
+                        setShowProject(showProject + 1);
+                      }
+                      setClickedButton('next')
+                    }}
+                    onAnimationComplete={(definiton) => {
+                      if(definiton === 'clicked') setClickedButton(null)
+                    }}
+                  >
+                    N E X T
+                  </motion.button>
+                </>
+              )}
+            </AnimatePresence>
+          </motion.div>
+        </Card>
+      </motion.div>
+
+      <motion.div
+        key={showProject}
+        className="md:w-1/2 mx-auto mt-5"
+        variants={popIn}
+        initial='hidden'
+        whileInView='visible'
+        transition={{
+          ease: 'linear',
+          duration: 0.3,
+          delayChildren: stagger(0.03, { startDelay: 0.3 }),
+        }}
+      >
+        <Card>
+          <RevealText
+            text={projectData[showProject].desc}
+          />
         </Card>
       </motion.div>
     </div>
